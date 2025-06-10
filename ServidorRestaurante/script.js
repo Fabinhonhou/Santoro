d3ea253c995f3b38e2d7746332b11356a1cda8b8
Beatriz <beatrizcoelhosilvap@gmail.com>
2025-06-10 11:21:23 -0300

    qlqr

--- ServidorRestaurante/script.js
+++ ServidorRestaurante/script.js
@@ -23,37 +23,34 @@ window.onscroll = function() {
 
 
 
+function alternarMenu() {
+  const menu = document.getElementById("menu");
+  menu.classList.toggle("aberto");
+}
 
-
-
-
-
-
-
-const mesas = document.querySelectorAll('.container div');  /* Captura todas as divs dentro do .container */
-const modal = document.getElementById("modal");  /* Captura o modal */
-const modalImage = document.getElementById("modal-image");  /* Captura a imagem dentro do modal */
-const closeButton = document.getElementById("close");  /* Captura o botão de fechar */
-
-
-mesas.forEach(mesa => {
-  mesa.addEventListener("click", function() {
-    const imageSrc = mesa.getAttribute('data-image');  /* Obtém o valor do atributo 'data-image' da mesa */
-    modalImage.src = imageSrc;  /* Define o atributo 'src' da imagem dentro do modal */
-    modal.style.display = "flex";  /* Exibe o modal (fica visível) */
-  });
-});
-
-
-closeButton.addEventListener("click", function() {
-  modal.style.display = "none";  /* Esconde o modal ao clicar no 'X' */
-});
-
-
-window.addEventListener("click", function(event) {
-  if (event.target == modal) {  /* Verifica se o clique foi fora da imagem */
-    modal.style.display = "none";  /* Esconde o modal */
+// Mudança de cor do botão ao rolar a página
+window.onscroll = function() {
+  var menuButton = document.querySelector('.menu-btn');
+  if (window.scrollY > 50) {
+      menuButton.classList.add('scrolled');
+  } else {
+      menuButton.classList.remove('scrolled');
   }
-});
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
